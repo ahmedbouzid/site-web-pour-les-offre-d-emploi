03f6eaf10a0498d9e436092c2afa978c62020810
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-15 12:24:45 +0200

    core ui configuration

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,29 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { CommonModule } from '@angular/common';
+/* Components */
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { CommonModule } from '@angular/common';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+/* Core ui Module  */
+import { ButtonModule } from '@coreui/angular';
+import { FormModule } from '@coreui/angular';
+
+
+import { IconModule, IconSetService } from '@coreui/icons-angular';
+
+/* Prime ng module  */
 import { PrimeNgModule } from '../primeng.module';
+
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchbarComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    PrimeNgModule
+    PrimeNgModule ,
+    ButtonModule ,
+    FormModule ,
+    IconModule,
+
+
   ],
-  exports : [HeaderComponent , FooterComponent]
+  providers : [IconSetService],
+  exports : [HeaderComponent , FooterComponent , SearchbarComponent]
 })
 export class SharedModule { }
